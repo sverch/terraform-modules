19b1051090445dfb84cf6e61d530024d221d0370
Shaun Verch <me@shaunverch.com>
2017-09-27 09:44:18 -0400

    Add aws_region as parameter

--- base_vpc/main.tf
+++ base_vpc/main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+    region = "${var.aws_region}"
+}
+
 resource "aws_vpc" "main" {
   cidr_block = "${var.cidr_block}"
 }
--- base_vpc/variables.tf
+++ base_vpc/variables.tf
@@ -1,3 +1,8 @@
+variable "aws_region" {
+    description = "The region to deploy to"
+    default = "us-east-1"
+}
+
 variable "cidr_block" {
     description = "The CIDR block of this VPC"
     default = "10.0.0.0/16"
