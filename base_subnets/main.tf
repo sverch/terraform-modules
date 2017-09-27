0db94141cf5035e5c3de7232138242c81b999f1a
Shaun Verch <me@shaunverch.com>
2017-09-27 16:53:15 -0400

    Explicitly set s3 as the state backend

--- base_subnets/main.tf
+++ base_subnets/main.tf
@@ -2,6 +2,10 @@ provider "aws" {
     region = "${var.aws_region}"
 }
 
+terraform {
+    backend "s3" {}
+}
+
 data "aws_vpc" "base" {
     tags = {
         environment = "${var.env_name}"
--- base_vpc/main.tf
+++ base_vpc/main.tf
@@ -2,6 +2,10 @@ provider "aws" {
     region = "${var.aws_region}"
 }
 
+terraform {
+    backend "s3" {}
+}
+
 resource "aws_vpc" "main" {
     cidr_block = "${var.cidr_block}"
 
