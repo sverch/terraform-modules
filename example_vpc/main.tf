730c3845eb149fc7f3b51bad3c15eb8dc28f2d34
Shaun Verch <me@shaunverch.com>
2017-10-08 23:14:33 -0400

    Add internet gateway to example vpc

--- example_vpc/main.tf
+++ example_vpc/main.tf
@@ -18,3 +18,22 @@ resource "aws_vpc" "example" {
         environment = "${var.environment_name}"
     }
 }
+
+resource "aws_internet_gateway" "example" {
+    vpc_id = "${aws_vpc.example.id}"
+
+    tags = {
+        # Need this for now, because awspec only searches by name.  See:
+        # https://github.com/k1LoW/awspec/issues/224
+        #
+        # Looks like https://github.com/k1LoW/awsrm is an attempt to fix this.
+        Name = "${var.environment_name}"
+        environment = "${var.environment_name}"
+    }
+}
+
+resource "aws_route" "example" {
+    route_table_id = "${aws_vpc.example.default_route_table_id}"
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.example.id}"
+}
--- example_vpc/spec/tests/default_spec.rb
+++ example_vpc/spec/tests/default_spec.rb
@@ -6,3 +6,8 @@ describe vpc(environment_name) do
   it { should exist }
   its('cidr_block') { should eq "10.0.0.0/16" }
 end
+
+describe internet_gateway(environment_name) do
+  it { should exist }
+  it { should be_attached_to(environment_name) }
+end
